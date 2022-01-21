03306a9959473b40d03ce6789b4fedf34eb69e00
Tamizhazhagan M <tamizhh1997@gmail.com>
2022-01-22 00:55:33 +0530

    udpate

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+const whitelist = ['http://localhost:3000']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+app.use(cors(corsOptions));
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/road-trip-planner'));
 
