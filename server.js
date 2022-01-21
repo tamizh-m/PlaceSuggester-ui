023e1dcb3ebc34da9f1862d40b3b0a485b32a922
Tamizhazhagan M <tamizhh1997@gmail.com>
2022-01-22 01:42:52 +0530

    udpate

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/road-trip-planner'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/road-trip-planner/'}),
 );
 
 // Start the app by listening on the default Heroku port
